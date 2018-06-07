7be936845db369fd682fe9d153494495cd3dd989
CanftIn <wwc7033@gmail.com>
2018-06-08 01:35:14 +0800

    add

---
+++ Chapter1/book1.1.2.scm
@@ -0,0 +1,14 @@
+(define size 2)
+
+(begin  (display "size=") 
+        (display size) (newline))
+
+(define pi 3.14)
+(define radius 10)
+(display (* pi (* radius radius)))
+(newline)
+
+(define circumference (* 2 pi radius))
+
+(display circumference)
+(newline)
--- Chapter1/test.scm
+++
@@ -1,1 +0,0 @@
-(begin (display "Hello,World!") (newline))
