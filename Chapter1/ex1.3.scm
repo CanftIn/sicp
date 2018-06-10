8368bd1757c0e2a37d33680ff71b99dc6bb4eb72
CanftIn <wwc7033@gmail.com>
2018-06-11 00:17:09 +0800

    add

---
+++ Chapter1/ex1.3.scm
@@ -0,0 +1,24 @@
+(define (three_big x y z)
+    (if (> x y)
+        (if (> y z)
+            y
+            z
+            )
+        ; y 较大
+        (if (> x z)
+            x
+            z
+            )))
+
+(define (bigger x y)
+    (if (> x y)
+        x
+        y))
+
+(define (smaller x y)
+    (if (> x y)
+        y
+        x))
+
+;(define (bigger) (bigger x y))
+(define (another-bigger x y z) (bigger (smaller x y) z))
