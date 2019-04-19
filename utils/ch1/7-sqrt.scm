d9e43ca43318610f72dcbaa92c8355164b99e2b1
CanftIn <wwc7033@gmail.com>
2019-04-20 01:19:12 +0800

    update

---
+++ utils/ch1/7-sqrt.scm
@@ -0,0 +1,16 @@
+(load "../utils/ch1/7-good-enough.scm")
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess (improve guess x))
+      (improve guess x)
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
