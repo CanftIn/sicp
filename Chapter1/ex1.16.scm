3458432ab425ae3f9c72d0cdb306d26a8008301e
CanftIn <wwc7033@gmail.com>
2019-04-20 16:15:30 +0800

    Create ex1.16.scm

---
+++ Chapter1/ex1.16.scm
@@ -0,0 +1,16 @@
+(define (fast-expt b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b n a)
+  (cond ((= n 0)
+         a)
+        ((even? n)
+         (expt-iter (square b)
+                    (/ n 2)
+                    a))
+        ((odd? n)
+         (expt-iter b
+                    (- n 1)
+                    (* b a)))))
+
+(define (square x) (* x x))
