e0a2103867a5102f5b6970d15b395871183ebfcc
CanftIn <wwc7033@gmail.com>
2019-04-20 15:46:22 +0800

    Update ex1.11.scm

--- Chapter1/ex1.11.scm
+++ Chapter1/ex1.11.scm
@@ -4,3 +4,15 @@
          (+ (f (- n 1))
             (* 2 (f (- n 2)))
             (* 3 (f (- n 3)))))))
+
+(define (f-i n)
+  (f-iter 2 1 0 0 n))
+
+(define (f-iter a b c i n)
+  (if (= i n)
+      c
+      (f-iter (+ a (* 2 b) (* 3 c))
+              a
+              b
+              (+ i 1)
+              n)))
