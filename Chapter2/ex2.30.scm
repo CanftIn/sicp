c8753bab43cbcc3e71f5fae9130cc4b2b0c0600b
CanftIn <wwc7033@gmail.com>
2019-04-30 17:55:39 +0800

    Update ex2.30.scm

--- Chapter2/ex2.30.scm
+++ Chapter2/ex2.30.scm
@@ -13,3 +13,15 @@
                      (list 1
                            (list 2 (list 3 4) 5)
                            6 7)))
+
+(define (square-tree-new tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree-new sub-tree)
+             (square sub-tree)))
+       tree))
+
+(define square-tree-new-test (square-tree-new
+                     (list 1
+                           (list 2 (list 3 4) 5)
+                           6 7)))
